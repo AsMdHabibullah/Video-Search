6a3df0b5809e26b8ec3f2f558d8a307dc70d79d2
asmdhabibullah <asmdhabibullah@yahoo.com>
2019-04-22 20:49:37 +0800

    Updates

---
+++ precache-manifest.1a3b106bd9334e2bfdebc992620e1791.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "65d5a2aeb2422a5565de",
+    "url": "/video-search/static/js/runtime~main.257bc5ca.js"
+  },
+  {
+    "revision": "8813247fbde22ed61ff8",
+    "url": "/video-search/static/js/main.6b48aaa3.chunk.js"
+  },
+  {
+    "revision": "381bd96f99f3fb8fb88d",
+    "url": "/video-search/static/js/2.c79a505c.chunk.js"
+  },
+  {
+    "revision": "8813247fbde22ed61ff8",
+    "url": "/video-search/static/css/main.015a7619.chunk.css"
+  },
+  {
+    "revision": "070725c8ce18e5ebb817e3b456ffcf03",
+    "url": "/video-search/index.html"
+  }
+];
